50da322b98d38f4ce64292876bf69301b4c09a65
fatma mohammed <om91327@gmail.com>
2025-08-19 15:28:38 +0300

    adding my Dashboard project

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const sidebar = document.querySelector(".sidebar");
+
+  menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+});
